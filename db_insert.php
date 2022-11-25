050e229d372dbab2c19dd73d64fe74d41d7ac999
JongKook <94429120+JongKookE@users.noreply.github.com>
2022-11-26 00:06:31 +0900

    Update db_insert.php

--- db_insert.php
+++ db_insert.php
@@ -2,7 +2,7 @@
     $server_name = "localhost";
     $user = "root";
     $password = "";
-    $db_name = "test";
+    $db_name = "fti";
     $connect = mysqli_connect($server_name, $user, $password, $db_name);
     
     $user_id = $_POST["login_id"];
@@ -18,8 +18,7 @@
         // DB 연결에 실패하면 에러 메시지를 출력하고 PHP 스크립트를 종료
     }
     
-    $insert_sql = "insert into user_info(id, password, name, email) values('$user_id', '$user_password','$user_name', '$user_email')";
-    $show_table = "select * from user_info";
+    $insert_sql = "insert into users(id, pass, name, email) values('$user_id', '$user_password','$user_name', '$user_email')";
 
     mysqli_query($connect, $insert_sql);
 
